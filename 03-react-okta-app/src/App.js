552326b45ebcbe2043551b541f979a3a7f01717f
Atanas Roussev <atanas@nextinterfaces.com>
2017-11-08 15:12:09 -0800

    react okta integration

--- 03-react-okta-app/src/App.js
+++ 03-react-okta-app/src/App.js
@@ -1,21 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
+import Home from './Home';
+
+const config = {
+  issuer: 'https://dev-977591.oktapreview.com/oauth2/default',
+  redirect_uri: window.location.origin + '/implicit/callback',
+  client_id: '0oacroeeg43id9AnQ0h7'
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Security issuer={config.issuer}
+                  client_id={config.client_id}
+                  redirect_uri={config.redirect_uri} >
+          <SecureRoute path='/' exact={true} component={Home}/>
+          <SecureRoute path='/implicit/callback' component={ImplicitCallback} />
+        </Security>
+      </Router>
     );
   }
 }
 
 export default App;
+
+
+
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
+//
+// const config = {
+//   issuer: 'https://dev-977591.oktapreview.com/oauth2/default',
+//   redirectUri: window.location.origin + '/implicit/callback',
+//   clientId: '0oacroeeg43id9AnQ0h7'
+// }
+//
+//
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+//
+// export default App;
