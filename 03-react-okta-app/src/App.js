87a3d422d17898800c6af4cb2de207dbbf4fd17c
Atanas Roussev <atanas@nextinterfaces.com>
2017-11-08 19:13:34 -0800

    login/logout

--- 03-react-okta-app/src/App.js
+++ 03-react-okta-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
 import Home from './Home';
 
@@ -16,8 +16,8 @@ class App extends Component {
         <Security issuer={config.issuer}
                   client_id={config.client_id}
                   redirect_uri={config.redirect_uri} >
-          <SecureRoute path='/' exact={true} component={Home}/>
-          <SecureRoute path='/implicit/callback' component={ImplicitCallback} />
+          <Route path='/' exact={true} component={Home}/>
+          <Route path='/implicit/callback' component={ImplicitCallback} />
         </Security>
       </Router>
     );
@@ -25,35 +25,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-// import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
-//
-// const config = {
-//   issuer: 'https://dev-977591.oktapreview.com/oauth2/default',
-//   redirectUri: window.location.origin + '/implicit/callback',
-//   clientId: '0oacroeeg43id9AnQ0h7'
-// }
-//
-//
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-//
-// export default App;
