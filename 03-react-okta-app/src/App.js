7c4e93962f6c5562f5932c012de8128a6080de10
Atanas Roussev <atanas@nextinterfaces.com>
2017-11-08 19:27:54 -0800

    reformat

--- 03-react-okta-app/src/App.js
+++ 03-react-okta-app/src/App.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Security, SecureRoute, ImplicitCallback} from '@okta/okta-react';
 import Home from './Home';
 
 const config = {
-  issuer: 'https://dev-977591.oktapreview.com/oauth2/default',
-  redirect_uri: window.location.origin + '/implicit/callback',
-  client_id: '0oacroeeg43id9AnQ0h7'
+    issuer: 'https://dev-977591.oktapreview.com/oauth2/default',
+    redirect_uri: window.location.origin + '/implicit/callback',
+    client_id: '0oacroeeg43id9AnQ0h7'
 }
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Security issuer={config.issuer}
-                  client_id={config.client_id}
-                  redirect_uri={config.redirect_uri} >
-          <Route path='/' exact={true} component={Home}/>
-          <Route path='/implicit/callback' component={ImplicitCallback} />
-        </Security>
-      </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <Security issuer={config.issuer}
+                          client_id={config.client_id}
+                          redirect_uri={config.redirect_uri}>
+                    <Route path='/' exact={true} component={Home}/>
+                    <Route path='/implicit/callback' component={ImplicitCallback}/>
+                </Security>
+            </Router>
+        );
+    }
 }
 
 export default App;
--- 03-react-okta-app/src/Home.js
+++ 03-react-okta-app/src/Home.js
@@ -1,32 +1,36 @@
 /// src/Home.js
 
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { withAuth } from '@okta/okta-react';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import {withAuth} from '@okta/okta-react';
 
-export default withAuth(class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { authenticated: null };
-    this.checkAuthentication = this.checkAuthentication.bind(this);
-    this.checkAuthentication();
-  }
+export default withAuth(
+    class Home extends Component {
+        constructor(props) {
+            super(props);
+            this.state = {authenticated: null};
+            this.checkAuthentication = this.checkAuthentication.bind(this);
+            this.checkAuthentication();
+        }
 
-  async checkAuthentication() {
-    const authenticated = await this.props.auth.isAuthenticated();
-    if (authenticated !== this.state.authenticated) {
-      this.setState({ authenticated });
+        async checkAuthentication() {
+            const authenticated = await this.props.auth.isAuthenticated();
+            if (authenticated !== this.state.authenticated) {
+                this.setState({authenticated});
+            }
+        }
+
+        componentDidUpdate() {
+            this.checkAuthentication();
+        }
+
+        render() {
+            if (this.state.authenticated === null) {
+                return null;
+            }
+            return this.state.authenticated ?
+                <button onClick={this.props.auth.logout}>Logout</button> :
+                <button onClick={this.props.auth.login}>Login</button>;
+        }
     }
-  }
-
-  componentDidUpdate() {
-    this.checkAuthentication();
-  }
-
-  render() {
-    if (this.state.authenticated === null) return null;
-    return this.state.authenticated ?
-      <button onClick={this.props.auth.logout}>Logout</button> :
-      <button onClick={this.props.auth.login}>Login</button>;
-  }
-});
+);
